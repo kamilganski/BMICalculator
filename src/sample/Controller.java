a37addcad1cf425f07ba0881cf9b06ff293baa9f
Kamil Gański <kamilganski2@gmail.com>
2018-11-21 21:22:20 +0100

    First run

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,51 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private TextField weight;
+
+    @FXML
+    private TextField height;
+
+    @FXML
+    private RadioButton women;
+
+    @FXML
+    private RadioButton man;
+
+    @FXML
+    private Label result;
+
+    @FXML
+    public void handleButtonAction(ActionEvent actionEvent) {
+        try {
+            Double w = new Double(weight.getText());
+            Double h = new Double(height.getText());
+
+            Double bmi = w * (h * h);
+
+            result.setText(String.format("%.2f", bmi));
+        } catch(NumberFormatException e) {
+            weight.setText("Wpisz poprawną wartość");
+            weight.selectAll();
+            weight.requestFocus();
+            height.setText("Wpisz poprawną wartość");
+            height.selectAll();
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,13 +11,13 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("BMI calculator");
+        primaryStage.setScene(new Scene(root, 350, 300));
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
+
         launch(args);
     }
 }
