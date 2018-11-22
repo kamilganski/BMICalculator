f77a2fd013f4e88d96efc24b61a299d0a897b895
Kamil Gański <kamilganski2@gmail.com>
2018-11-22 22:00:51 +0100

    Added scale to weight

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -10,6 +10,8 @@ import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static javafx.scene.paint.Color.*;
+
 public class Controller implements Initializable {
 
     @FXML
@@ -28,6 +30,9 @@ public class Controller implements Initializable {
     private Label result;
 
     @FXML
+    private Label value;
+
+    @FXML
     public void handleButtonAction(ActionEvent actionEvent) {
         try {
             Double w = new Double(weight.getText());
@@ -41,6 +46,32 @@ public class Controller implements Initializable {
             }
 
             result.setText(String.format("%.2f", bmi));
+
+            if (bmi <= 16) {
+                value.setText("Wygłodzenie");
+                value.setTextFill(RED);
+            } else if (bmi > 16 && bmi <= 16.99) {
+                value.setText("Wychudzenie");
+                value.setTextFill(RED);
+            } else if (bmi > 17 && bmi <= 18.49) {
+                value.setText("Niedowaga");
+                value.setTextFill(ORANGE);
+            } else if (bmi > 18.5 && bmi <= 24.99) {
+                value.setText("Wartość prawidłowa");
+                value.setTextFill(GREEN);
+            } else if (bmi > 25 && bmi <= 29.99) {
+                value.setText("Nadwaga");
+                value.setTextFill(ORANGE);
+            } else if (bmi > 30 && bmi <= 34.99) {
+                value.setText("I stopień otyłości");
+                value.setTextFill(RED);
+            } else if (bmi > 35 && bmi <= 39.99) {
+                value.setText("II stopień otyłości");
+                value.setTextFill(RED);
+            } else if (bmi > 40) {
+                value.setText("Otyłość skrajna");
+                value.setTextFill(RED);
+            }
 
         } catch(NumberFormatException e) {
             weight.setPromptText("Wpisz poprawną wartość");
